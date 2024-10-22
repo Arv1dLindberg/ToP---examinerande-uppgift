ec118211af98cc531354ae56059f66cbf36cde6e
arpl <arpl@APPL>
2024-10-22 05:27:36 +0200

    Add project files.

---
+++ ToP - examinerande uppgift/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToP___examinerande_uppgift
+{
+    internal class Person
+    {
+    }
+
+    class Tjuv : Person 
+    {
+    }
+
+    class Polis : Person
+    {
+    }
+
+    class Medborgare : Person
+    {
+    }
+}
---
+++ ToP - examinerande uppgift/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ToP___examinerande_uppgift
+{
+    internal class Program
+    {
+        // City Size
+        public const int cityWidth = 100;
+        public const int cityHeight = 25;
+
+        static void Main(string[] args)
+        {
+            bool codeIsWorking = true;
+            while(codeIsWorking)
+            {
+
+            }
+        }
+    }
+}
