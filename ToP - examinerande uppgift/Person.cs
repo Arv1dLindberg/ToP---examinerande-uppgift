7a5e32ee6f6d08125f62f7720dc1e5e365f79e4c
Arvid Lindberg <pl.arvid@gmail.com>
2024-10-22 21:35:21 +0200

    Skrev metoden CreateCityWalls som ritar ut gränserna av staden, och metoden CreateCity för platsen inom murarna.
    Skapade variabler för personernas koordinater, vilken riktning de färdas i, och namn.
    
    Skrev metoden PlacePeople för att skapa och placera ut en specifik mängd T, P och M, som slumpmässigt får en riktning enligt x och y-axis.
    
    Gjorde metoden MovePeople som får TPM att uppdatera sina positioner med en ruta i den riktning de är tilldelade och teleportera till motsatt x/y-axis om de rör sig utanför murarna.
    
    Skapade metoden ClearPosition för att radera TPM från dess förra koordinater.
    
    Skapade metoden PeoplePosition som gör att man ser TPM i deras uppdaterade koordinater efter de har förflyttat sig.
    
    Skrev metoden GenerateName som slumpmässigt väljer ut och returnerar ett namn från en lista med namn på mina kompisar och familj.

--- ToP - examinerande uppgift/Person.cs
+++ ToP - examinerande uppgift/Person.cs
@@ -8,17 +8,29 @@ namespace ToP___examinerande_uppgift
 {
     internal class Person
     {
+        public int X { get; set; } // X coordinate
+        public int Y { get; set; } // Y coordinate
+        public int Xdirection { get; set; } // Direction along the X-axis
+        public int Ydirection { get; set; } // Direction along the Y-axis
+        public string Name { get; set; }
+        public char Symbol => GetSymbol(); // Symbol representing people
+        protected virtual char GetSymbol() => ' ';
+
+        
+    }
+    // Arv av Person
+    class Thief : Person 
+    {
+        protected override char GetSymbol() => 'T';
     }
 
-    class Tjuv : Person 
+    class Police : Person
     {
+        protected override char GetSymbol() => 'P';
     }
 
-    class Polis : Person
+    class Citizen : Person
     {
-    }
-
-    class Medborgare : Person
-    {
+        protected override char GetSymbol() => 'M';
     }
 }
--- ToP - examinerande uppgift/Program.cs
+++ ToP - examinerande uppgift/Program.cs
@@ -6,13 +6,130 @@
         public const int cityWidth = 100;
         public const int cityHeight = 25;
 
-        static void Main(string[] args)
-        {
-            bool codeIsWorking = true;
-            while(codeIsWorking)
-            {
+        private static Random rndm = new Random();
 
+        static void Main()
+        {
+            char[,] city = CreateCity();
+
+            List<Person> people = new List<Person>(); // Creates a list that holds elements of Person
+            people.AddRange(PlacePeople<Thief>(10));   // Generates a specified amount of Thieves
+            people.AddRange(PlacePeople<Police>(5));   // Generates a specified amount of Police
+            people.AddRange(PlacePeople<Citizen>(30)); // Generates a specified amount of Citizens
+
+            // Repeats the program until manually stopped
+            while (true)
+            {
+                Thread.Sleep(1000);
+                ClearPosition(city, people);
+                MovePeople(people);
+                PeoplePosition(city, people);
+                Console.Clear();
+                CreateCityWalls(city);
             }
+        }
+
+        // Creates the city boundaries
+        static void CreateCityWalls(char[,] city)
+        {
+            Console.WriteLine(new string('X', cityWidth));
+            for (int i = 0; i < cityHeight; i++)
+            {
+                Console.Write("X");
+                for (int j = 1; j < cityWidth - 1; j++)
+                    Console.Write(city[i, j]);
+                Console.WriteLine("X");
+            }
+            Console.WriteLine(new string('X', cityWidth));
+        }
+
+        // Creates the space that the people will move around in
+        static char[,] CreateCity()
+        {
+            char[,] city = new char[cityHeight, cityWidth];
+
+            for (int i = 0; i < cityHeight; i++)
+                for (int j = 0; j < cityWidth; j++)
+                    city[i, j] = ' ';
+            return city;
+        }
+
+        /* Creates and places a specified amount of people within the city and randomly assigns them one of 7 directions
+         * (left, right, diagonally left, diagonally right, up, down, and still
+         */
+        static List<T> PlacePeople<T>(int count) where T : Person, new()
+        {
+            List<T> people = new List<T>();
+            for (int i = 0; i < count; i++)
+            {
+                int x = rndm.Next(1, cityWidth - 1);
+                int y = rndm.Next(1, cityHeight - 1);
+
+                T person = new T
+                {
+                    X = x,
+                    Y = y,
+                    Xdirection = rndm.Next(-1, 2),
+                    Ydirection = rndm.Next(-1, 2)
+                };
+
+                people.Add(person);
+            }
+            return people;
+        }
+
+        // Gives the person's their direction and moves them
+        static void MovePeople(List<Person> people)
+        {
+            foreach (var person in people)
+            {
+                // Updates X position
+                person.X += person.Xdirection;
+
+                // If a person moves outside the X walls they will come back on the opposite X-axis
+                if (person.X <= 0)
+                {
+                    person.X = Program.cityWidth - 1;
+                }
+                else if (person.X >= Program.cityWidth)
+                {
+                    person.X = 1;
+                }
+
+                // Updates Y position
+                person.Y += person.Ydirection;
+
+                // If a person moves outside the Y walls they will come back on the opposite Y-axis
+                if (person.Y < 0)
+                {
+                    person.Y = Program.cityHeight - 1;
+                }
+                else if (person.Y >= Program.cityHeight)
+                {
+                    person.Y = 0;
+                }
+            }
+        }
+
+        // Clears the person's previous position
+        static void ClearPosition(char[,] city, List<Person> people)
+        {
+            foreach (var person in people)
+                city[person.Y, person.X] = ' ';
+        }
+
+        // Updates person's position in the city
+        static void PeoplePosition(char[,] city, List<Person> people)
+        {
+            foreach (var person in people)
+                city[person.Y, person.X] = person.Symbol;
+        }
+        
+        // List of names that are randomly returned
+        static string GenerateName()
+        {
+            string[] Names = { "Anthony", "Mikael", "Sean", "Nikita", "Arya", "Jakob", "Natan", "Joel", "Noa", "Melker", "Nelly", "Ebba", "Wera", "Christin", "Astrid", "Ylva" };
+            return Names[rndm.Next(Names.Length)];
         }
     }
 }
